8007e03ecc2d0f8bea02515ccb8af00cc1ed4958
Niels P <niels.poelder@gmail.com>
2019-11-26 16:21:54 +0100

    Init Commit

---
+++ Assets/Script/Lesson1/Editor/LevelEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Level))]
+public class LevelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // base.OnInspectorGUI();
+        Level myLevel = (Level)target;
+
+        myLevel.experience = EditorGUILayout.IntField("Experience", myLevel.experience);
+        EditorGUILayout.LabelField("Level", myLevel.level.ToString());
+    }
+}
